1b8d889d004d330820921aa347d318c02aeebbf4
Jose Neil Silagan <neilsilagan@gmail.com>
2025-06-01 19:05:48 +0800

    Add preliminary workout drill datas in startWorkout

---
+++ controllers/startWorkout.js
@@ -0,0 +1,7 @@
+import workoutService from "../services/workouts.js";
+
+const currentWorkout = localStorage.getItem("currentWorkout");
+
+const workoutDrills = await workoutService.getWorkoutDrills(currentWorkout);
+
+console.log(workoutDrills);
