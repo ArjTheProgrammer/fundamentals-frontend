5d27f4f536c21a990c31a1356d7fd5cb0a561172
Jose Neil Silagan <neilsilagan@gmail.com>
2025-06-01 21:02:54 +0800

    Add finish dialog box

--- controllers/startWorkout.js
+++ controllers/startWorkout.js
@@ -5,3 +5,5 @@ const currentWorkout = localStorage.getItem("currentWorkout");
 const workoutDrills = await workoutService.getWorkoutDrills(currentWorkout);
 
 console.log(workoutDrills);
+
+const finishDialog = document.getElementById("finish");
